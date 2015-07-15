c7c772914e8a5567ba83ab316967049d61a92fd0
cnrdh <cnrdh@users.noreply.github.com>
2015-07-15 17:17:06 +0200

    Fixed password leak and lookup user in Person API

--- ui/auth/LoginController.js
+++ ui/auth/LoginController.js
@@ -2,21 +2,39 @@
 
 var LoginController = function ($scope, $http, $location, npolarApiConfig, NpolarApiUser, NpolarApiSecurity) {
 
+  // Login and store user (jwt, name, username, email, uri, uuid, exp, systems)
   $scope.login = function() {
     if (!$scope.user.username || !$scope.user.password) {
       return false;
     }
+    // TODO force https
     var url = npolarApiConfig.base+"/user/authenticate/";
 
     var req = { method: "GET", url: url,
       headers: { "Authorization": "Basic " + NpolarApiSecurity.basicToken($scope.user) }
     };
     $http(req).success(function(data) {
+      
+      var token = NpolarApiSecurity.decodeJwt(data.token);
+      
+      $scope.user = { name: $scope.user.username, username: $scope.user.username, jwt: data.token };
+      
+      // Merge user with data from the Person API
+      $http.get(token.uri).success(function(person) {
+        $scope.user.name = person.first_name+" "+person.last_name;
+        $scope.user.email = person.email;
+        $scope.user.uri = token.uri;
+        $scope.user.uuid = person.uuid;
+        $scope.user.exp = token.exp;
+        $scope.user.systems = token.systems;
 
-        $scope.user.jwt = data.token;
-        $scope.user.name = $scope.user.username;
         NpolarApiUser.setUser($scope.user);
-
+        
+      }).error(function() {
+        // Person API failed, still valid user
+        NpolarApiUser.setUser($scope.user);
+      });
+      
     }).error(function(error){
       console.error(error);
       $scope.logout();

57dd427ad73fe0c14e2af3434cb4bb412ee8baf0
cnrdh <cnrdh@users.noreply.github.com>
2015-08-25 15:04:53 +0200

    Simplified messages

--- ui/message/MessageController.js
+++ ui/message/MessageController.js
@@ -1,18 +1,67 @@
 'use strict';
 
 // @ngInject
-var MessageController = function ($scope, $route, $http, $location, $rootScope, npolarApiConfig, NpolarApiUser, NpolarApiSecurity, NpolarApiMessage) {
+var MessageController = function ($scope, $route, $http, $location, $mdToast, npolarApiConfig, NpolarApiUser, NpolarApiSecurity, NpolarApiMessage) {
   
-  NpolarApiMessage.on("npolar-api-info", function(message) {
-    console.log("<- npolar-api-info", message);
-    $scope.info = message;
-    $scope.error = null;
+  var flashError = function(message) {
+
+    let explanation = "";
+    
+    if (message.body && message.body.error && message.body.error.explanation ) {
+      explanation = message.body.error.explanation;
+    } else {
+      explanation = message;
+    }
+    
+    $mdToast.show({
+      controller: 'ToastCtrl',
+      templateUrl: 'angular-npolar/ui/message/_message_toast.html',
+      hideDelay: 30000,
+      action: "OK",
+      locals: { message: message, explanation: explanation },
+      position: "top left"
+    }).then(function() {
+      $route.reload();
+    });
+  };
+  
+  var flashInfo = function(message) {
+    
+    let explanation = "";
+    
+    explanation = message;
+    
+    
+    $mdToast.show({
+      controller: 'ToastCtrl',
+      templateUrl: 'angular-npolar/ui/message/_message_toast.html',
+      hideDelay: 5000,
+      action: "OK",
+      locals: { message: message, explanation: explanation },
+      position: "bottom right"
+    }).then(function() {
+      $route.reload();
+    });
+  };
+  
+  
+  NpolarApiMessage.on("npolar-api-info", function(response) {
+    console.log(response);
+    if ("POST" === response.method || "PUT" === response.method) {
+      flashInfo(`Saved document at ${ new Date(response.time).toLocaleTimeString() }`);
+    }
+  });
+  
+  NpolarApiMessage.on("npolar-login", function(user) {
+    flashInfo(`${user.name} logged in`);
+  });
+  
+  NpolarApiMessage.on("npolar-logout", function(user) {
+    flashInfo(`${user.name} logged out`);    
   });
     
   NpolarApiMessage.on("npolar-api-error", function(message) {
-    console.log("<- npolar-api-error", message);
-    $scope.info = null;
-    $scope.error = message;
+    flashError(message);
   });
   
 };

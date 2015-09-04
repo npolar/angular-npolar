e25a081ff54a6cd929e0d14a0a7f052fc7e34d2e
Anders Bälter <anders.balter@npolar.no>
2015-09-04 13:43:28 +0200

    Added angular-route dependency

--- src/ui/auth/LoginController.js
+++ src/ui/auth/LoginController.js
@@ -4,24 +4,24 @@
  * @ngInject
  */
 var LoginController = function ($scope, $http, $route, $location, $rootScope, $timeout, $interval, npolarApiConfig, NpolarApiUser, NpolarApiMessage, NpolarApiSecurity) {
-  
+
   const message = NpolarApiMessage;
-  
+
   const authenticateUri = "https://"+ npolarApiConfig.base.split("//")[1] +"/user/authenticate";
-  
-  $scope.security = NpolarApiSecurity; 
-  
+
+  $scope.security = NpolarApiSecurity;
+
   // @var response Gouncer response body
   var getErrorMessage = function(response) {
-    
+
     if (null === response) {
-      
+
       return message.getMessage(response);
-    
+
     } else {
-        
+
       var time = new Date(Date.now()).toJSON();
-      
+
       return { status: response.status,
         method: "GET",
         uri: authenticateUri,
@@ -29,16 +29,16 @@ var LoginController = function ($scope, $http, $route, $location, $rootScope, $t
         body: { error: { explanation: response.error } }
       };
     }
-    
+
   };
-     
+
   // After login success: store user in session
   $scope.onLogin = function(data) {
-    
+
     var token = NpolarApiSecurity.decodeJwt(data.token);
     var expires = new Date(1000*token.exp).toISOString();
     var now = Date.now();
-    
+
     $scope.user = { name: token.name || $scope.user.username,
       email: $scope.user.username,
       username: $scope.user.username,
@@ -48,35 +48,35 @@ var LoginController = function ($scope, $http, $route, $location, $rootScope, $t
       expires: expires,
       systems: token.systems
     };
-    
+
     NpolarApiUser.setUser($scope.user);
-    
+
     // Merge user name, email, and uuid from the Person API
     // FIXME
     if (/^http/.test(token.uri)) {
-      
+
       $http.get(token.uri).success(function(person) {
-        
+
         $scope.user.name = person.first_name+" "+person.last_name;
         $scope.user.email = person.email;
         $scope.user.uuid = person.uuid;
-        
+
         NpolarApiUser.setUser($scope.user);
-        
+
         message.emit("npolar-login", $scope.user);
-        
-        
+
+
       });
     } else {
-      message.emit("npolar-login", $scope.user); 
+      message.emit("npolar-login", $scope.user);
     }
     $route.reload();
-    
+
   };
-  
+
   // Login (using username and password)
   $scope.login = function() {
-    
+
     if (!$scope.user.username || !$scope.user.password) {
       return false;
     }
@@ -91,18 +91,18 @@ var LoginController = function ($scope, $http, $route, $location, $rootScope, $t
       response => { message.emit("npolar-api-error", "Login failed"); }
     );
   };
-  
+
   $scope.logout = function() {
     var who = NpolarApiUser.getUser();
     message.emit("npolar-logout", who);
-    
+
     $scope.user = {};
     NpolarApiSecurity.removeUser();
     $location.path('/');
     $route.reload();
-    
+
   };
-  
+
 };
 
 module.exports = LoginController;

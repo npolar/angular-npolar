70f88d26ceba25ee86036cb2fd83955b1646815f
cnrdh <cnrdh@users.noreply.github.com>
2015-10-19 15:11:50 +0200

    Simplified

--- src/api/controller/BaseController.js
+++ src/api/controller/BaseController.js
@@ -13,34 +13,10 @@ var _ = require('lodash');
 var BaseController = function($scope, $location, $log, $route, $routeParams, $window, $controller, $http,
   npolarApiConfig, NpolarApiMessage, NpolarApiSecurity, NpolarApiUser, NpolarApiResource) {
   
-  const authenticateUri = "https://"+ npolarApiConfig.base.split("//")[1] +"/user/authenticate";
-
   let init = function() {
-    $scope.base = npolarApiConfig.base;
-    //$scope.environment = npolarApiConfig.environment;
-    //$scope.lang = npolarApiConfig.lang;
-    //$scope.user = NpolarApiSecurity.getUser();
     $scope.security = NpolarApiSecurity;
   };
   
-  $scope.refreshJwt = function() {
-    let request = { method: "GET", url: authenticateUri,
-      headers: { "Authorization": `Bearer ${ $scope.security.getUser().jwt }` }
-    };
-    $log.debug(request);
-    
-    $http(request).success(response => {
-        
-        //$log.debug(response);
-        //NpolarApiMessage.emit("npolar-api-info", 'JWT refreshed');
-        
-      }).error(response => {
-      
-        // NpolarApiMessage.emit("npolar-api-error", 'Session expired, please log in again');
-      }
-    );
-  };
-
   // Show action, ie. fetch document and inject into scope
   $scope.show = function() {
 
@@ -56,21 +32,6 @@ var BaseController = function($scope, $location, $log, $route, $routeParams, $wi
     });
   };
 
-  // Deprecated
-  //$scope.getLang = function() {
-  //  return $scope.lang;
-  //};
-  //
-  //$scope.setLang = function(lang) {
-  //  $scope.lang = lang;
-  //  $scope.title = $scope.getTitle(lang);
-  //};
-  //
-  //$scope.getTitle = function(lang) {
-  //  return _.where($scope.document.titles,
-  //    { lang: lang }
-  //  )[0].text || $scope.document.titles[0].text;
-  //};
   $scope.showNext = function() {
     if (!$scope.feed) {
       return false;
@@ -82,10 +43,10 @@ var BaseController = function($scope, $location, $log, $route, $routeParams, $wi
     if (!$scope.feed.links) {
       return;
     }
-    console.debug($scope.feed.links);
+
     let nextLink = $scope.feed.links.find(link => { return (link.rel === "next"); });
     if (nextLink.href) {
-        $http.get(nextLink.href.replace(/^https?:/, "")).success(function(response) {
+      $http.get(nextLink.href.replace(/^https?:/, '')).success(function(response) {
         response.feed.entries = $scope.feed.entries.concat(response.feed.entries);
         $scope.feed = response.feed;
       });
@@ -94,6 +55,7 @@ var BaseController = function($scope, $location, $log, $route, $routeParams, $wi
   
 
   init();
+  
 };
 
 module.exports = BaseController;

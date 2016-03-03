ed8f1efd45ce5cdde3d35cbd4212643164204083
Anders Bälter <anders.balter@npolar.no>
2016-03-03 17:36:50 +0100

    minor fixes

--- src/ui/filters/bytes.js
+++ src/ui/filters/bytes.js
@@ -2,17 +2,17 @@
 
 module.exports = () => {
   'ngInject';
-  
+
   // From https://gist.github.com/thomseddon/3511330
-  return function(bytes, precision=1) {
+  return function(bytes, precision = 1) {
 
     if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) {
-        return '';
+      return '';
     }
     let units = ['bytes', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb'];
     let number = Math.floor(Math.log(bytes) / Math.log(1024));
-    
-	return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +' '+ units[number];
-  }
-  
+
+    return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
+  };
+
 };

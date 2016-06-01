efc56061f094f69851727bebdd4ecf6ea8b3c6c5
cnrdh <cnrdh@users.noreply.github.com>
2016-06-01 15:00:08 +0200

    Added ESRI:53032

--- src/ui/map/esri/NpolarEsriLeaflet.js
+++ src/ui/map/esri/NpolarEsriLeaflet.js
@@ -5,14 +5,19 @@ let L = require('leaflet');
 let EsriLeaflet = require('esri-leaflet');
 let Proj4Leaflet = require('proj4leaflet');
 require('leaflet-fullscreen');
+require('leaflet.heat/dist/leaflet-heat');
 
-// Injects esri and Proj4Leaflet plugins into Leaflet's L
+// @todo? WMTS...?
+// http://kartena.github.io/Proj4Leaflet/examples/wmts/
+// http://kartena.github.io/Proj4Leaflet/examples/wmts/script.js
+// Using-LDS-XYZ-tile-services-in-Leaflet-and-OpenLayers.pdf
+
+// Inject esri and Proj4Leaflet plugins into Leaflet's L
 L.esri = EsriLeaflet;
 L.Proj = Proj4Leaflet;
-
 L.Icon.Default.imagePath = '/assets/images';
 
-let NpolarEsriLeaflet = function($http) {
+let NpolarEsriLeaflet = function($http, $location, NpolarMessage) {
   'ngInject';
 
   let self = this;
@@ -20,36 +25,73 @@ let NpolarEsriLeaflet = function($http) {
   this.element = 'npolar-esri-leaflet-map'; // The map's html element @id
 
   this.base = '//geodata.npolar.no/arcgis/rest/services';
+  
+  this.uri = function(arg={ epsg: '53032'}) {
+    let uri = self.base;
+    
+    if (arg && arg.epsg) {
+      let epsg = parseInt(arg.epsg);
+      
+      if (epsg === 25833) {
+        uri += `/Basisdata_Intern/NP_Nordomraadene_WMTS_25833/MapServer`;   
+      } else if (epsg === 9999) {
+        uri += `/inspire1/NP_TopoArktis_UPSN_CLX/MapServer`;
+      } else if (epsg === 53032) {
+        uri += `/Basisdata_Intern/NP_Verden_WMTS_53032/MapServer`;
+      } else if (epsg === 4326) {
+        uri += '';  
+      } else {
+        console.error(`Unsupported EPSG ${epsg}`);
+      }
+      //console.log('epsg', epsg, '->', uri);
+    }
+    
+    return uri;
+  };
 
   // @return instance of Proj4Leaflet CRS (L.Proj.CRS)
   this.crsFactory = function(uri) {
+    
+    console.debug(uri, this.base);
+    
     let crs;
     let path = uri.split(`${self.base}`)[1];
+    
+    if ($location.search().debug) {
+      console.log(`${this.base}${path} debug info`);
+      $http.get(`${this.base}${path}?f=json`).then(r => {
+        console.log('spatialReference', r.data.spatialReference.wkid);
+        let lods = r.data.tileInfo.lods;
+        console.log('resolutions', lods.map(l => l.resolution));
+        console.log('scales', lods.map(l => l.scale));
+        console.log('levels', lods.map(l => l.level));
+      });    
+    }
+    
 
     if (path === `/Basisdata_Intern/NP_Nordomraadene_WMTS_25833/MapServer`) {
-      crs = self.UTM_33N_CRS();
+      crs = self.UTM_33N_CRSFactory();
 
     } else if (path === `/inspire1/NP_TopoArktis_UPSN_CLX/MapServer`) {
-      crs = self.UPSN_CRS();
-
+      crs = self.UPSN_CRSFactory();
+    
+    } else if (path === `/Basisdata_Intern/NP_Verden_WMTS_53032/MapServer`) {
+      crs = self.WMTS_53032_CRSFactory();
+      
+    } else if (/102016/.test(path)) {
+      crs = self.EPSG_102016_CRSFactory();
+      
     } else {
-      console.debug('EPSG:4326 <- '+uri);
       crs = new L.Proj.CRS("EPSG:4326","+proj=longlat +datum=WGS84 +no_defs");
     }
-    console.debug(crs);
-    $http.get(this.base + path).then(r =>  {
-      // noop
-    }, error => {
-      crs = null;
-      console.log(error);
-    });
     return crs;
   };
 
   // Polar stereographic north / WGS84
   // Projection definition for EPSG:32661 from http://epsg.io/32661.js
   // Resolutions from the tileInfo in https://geodata.npolar.no/arcgis/rest/services/inspire1/NP_TopoArktis_UPSN_CLX/MapServer?f=json
-  this.UPSN_CRS = function() {
+  this.UPSN_CRSFactory = function() {
+        
     return new L.Proj.CRS(
       'EPSG:32661',
       '+proj=stere +lat_0=90 +lat_ts=90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs ',
@@ -73,7 +115,7 @@ let NpolarEsriLeaflet = function($http) {
 
   // UTM zone 33N / ETRS89
   // Projection definition for EPSG:25833 from http://epsg.io/25833.js
-  this.UTM_33N_CRS = function() {
+  this.UTM_33N_CRSFactory = function() {
     return new L.Proj.CRS(
       'EPSG:25833',
       '+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs',
@@ -99,18 +141,64 @@ let NpolarEsriLeaflet = function($http) {
       }
     );
   };
-
+  
+  // ESRI:53032
+  // Sphere Azimuthal Equidistant
+  this.WMTS_53032_CRSFactory = function () {
+    let resolutions = [86698.8400643468, 43349.4200321734, 21674.7100160867, 10837.35500804335, 5418.677504021675, 2709.3387520108377, 1354.6693760054188, 677.3346880027094, 338.6673440013547];
+    let scales = [327680000, 163840000, 81920000, 40960000, 20480000, 10240000, 5120000, 2560000, 1280000];
+    let origin = [-21986016.870795302, 21986016.870795317];
+    
+    return new L.Proj.CRS(
+      'EPSG:53032', // or ESRI:53032?
+      '+proj=aeqd +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +a=6371000 +b=6371000 +units=m +no_defs',
+      {
+        transformation: new L.Transformation(1, -21986016.870795302, -1, 21986016.870795317),
+        resolutions,
+        origin
+      }
+    );
+  };
+  
+  this.EPSG_102016_CRSFactory = function () {
+    let resolutions = [43349.4200321734, 21674.7100160867, 10837.35500804335, 5418.677504021675, 2709.3387520108377];
+    let scales = [163840000, 81920000, 40960000, 20480000, 10240000];
+    let levels = [0, 1, 2, 3, 4];
+    let origin = [-20004100, 20004100];
+    
+    return new L.Proj.CRS(
+      'EPSG:102016', //http://epsg.io/102016.js
+      '+proj=aeqd +lat_0=90 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs',
+      {
+        transformation: new L.Transformation(1, -20004100, -1, 20004100),
+        resolutions,
+        //scales,
+        origin,
+        levels
+      }
+    );
+  };
+  
   this.esriBaseUri = function() {
     return `${self.base}/inspire1/NP_TopoArktis_UPSN_CLX/MapServer`;
   };
-
+  
+  this.heatLayerFactory = function(points, heatConfig = {
+    minOpacity: 0.9,
+    useLocalExtrema: true,
+    scaleRadius: true,
+    radius: 6,
+    }) {
+    return L.heatLayer(points, heatConfig);
+  };
+  
   this.tileLayerFactory = function(esriBase = self.esriBaseUri(), config = {
     url: esriBase,
-    maxZoom: 7,
-    minZoom: 2,
+    //maxZoom: 7,
+    //minZoom: 2,
     continuousWorld: true,
     attribution: null }) {
-
+    
     return new L.esri.tiledMapLayer(config);
   };
 
@@ -123,11 +211,18 @@ let NpolarEsriLeaflet = function($http) {
 
     let map = new L.Map(self.element, mapConfig);
     map.addLayer(self.tileLayerFactory(esriBase, tileLayerConfig));
-    map.setView([80, 0], 3);
+    map.setView([80, 0], 0);
+    
+    // Disable zoom handlers.
+    map.touchZoom.disable();
+    map.doubleClickZoom.disable();
+    map.scrollWheelZoom.disable();
+    map.keyboard.disable();
+
 
     return map;
   };
-
+  
   return this;
 };
 

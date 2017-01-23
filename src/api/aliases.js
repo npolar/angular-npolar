e59e8bb332d2709585904c5814d7550a5c217396
cnrdh <cnrdh@users.noreply.github.com>
2017-01-23 15:50:47 +0100

    Login with real name or alias :)

--- src/api/aliases.js
+++ src/api/aliases.js
@@ -1,20 +1,20 @@
 'use strict';
 
-// hudson
-// lydersen
-// Alias to official email address
-// Use name property for people/entities without official email (ie. not in npolarPeople)
+// Aliases => (official) email address
+// Use name property only for people/entities without official email (ie. not in npolarPeople)
 
 let aliases = [
   { email: 'external@data.npolar.no', alias: ['external@data.npolar.no'], name: 'System'},
-  { email: 'stein.tronstad@npolar.no', alias: ['stein@npolar.no', 'steint@npolar.no']},
+  { email: 'stein.tronstad@npolar.no', alias: ['stein@npolar.no', 'steint@npolar.no', 'stein', 'steint', 'st', 's']},
   { email: 'are.bjordal@npolar.no', alias: ['are@npolar.no', 'are']},
   { email: 'polona.itkin@npolar.no', alias: ['polona@npolar.no', 'polona']},
   { email: 'ermias.beyene.tesfamariam@npolar.no', alias: ['ermias@npolar.no', 'ermias.tesfamariam@npolar.no', 'ermias']},
-  { email: 'conrad.helgeland@npolar.no', alias: ['conrad@npolar.no', 'conrad']},
-  { email: 'ruben.dens@npolar.no', alias: ['ruben@npolar.no', 'ruben']},
+  { email: 'conrad.helgeland@npolar.no', alias: ['conrad@npolar.no', 'conrad', 'ch', 'c']},
+  { email: 'ruben.dens@npolar.no', alias: ['ruben@npolar.no', 'ruben', 'rd', 'r']},
   { email: 'dag.vongraven@npolar.no', alias: ['vongraven@npolar.no']},
   { email: 'trevor.lovett@npolar.no', name: 'Trevor Lovett', alias: ['trevor@npolar.no', 'trevor']},
+  { email: 'christian.lydersen@npolar.no', alias: ['lydersen@npolar.no', 'lydersen']},
+  { email: 'stephen.hudson@npolar.no', alias: ['hudson@npolar.no', 'hudson']},
   { email: 'cesar.deschamps.berger@npolar.no', name: 'César Deschamps-Berger', alias: ['cesar@npolar.no', 'cesar']}
 ];
 
--- src/ui/auth/LoginController.js
+++ src/ui/auth/LoginController.js
@@ -1,7 +1,34 @@
 'use strict';
 
-var LoginController = function ($scope, $route, Gouncer, NpolarMessage, NpolarApiSecurity) {
+function LoginController($scope, $route,
+  npolarPeople, npolarAliases, Gouncer, NpolarMessage, NpolarApiSecurity) {
   'ngInject';
+
+  let ctrl = this;
+
+  ctrl.emailFromName = (name, people=npolarPeople.people) => {
+
+    let person = (people||[]).find(p => {
+      return (`${p.first_name} ${p.last_name}`.toLowerCase() === name.toLowerCase());
+    });
+
+    if (person && person.email) {
+      return person.email;
+    } else {
+      return name;
+    }
+  };
+
+  ctrl.emailFromAlias = (alias, aliases=npolarAliases) => {
+    let m = (aliases||[]).find(p => {
+      return (p.alias && p.alias.includes(alias.toLowerCase())); // assumes aliases ARE already in lowercase
+    });
+    if (m && m.email) {
+      return m.email;
+    } else {
+      return alias;
+    }
+  };
 
   $scope.security = NpolarApiSecurity;
 
@@ -9,10 +36,8 @@ var LoginController = function ($scope, $route, Gouncer, NpolarMessage, NpolarAp
 
   // After login: store user and JWT in local storage
   $scope.onLogin = function(response) {
-
     NpolarApiSecurity.setJwt(response.data.token);
     NpolarMessage.login(NpolarApiSecurity.getUser());
-
   };
 
   $scope.onLoginError = function(response) {
@@ -21,14 +46,23 @@ var LoginController = function ($scope, $route, Gouncer, NpolarMessage, NpolarAp
 
   // Login (using username and password)
   $scope.login = function(email, password) {
+    // Login using real name
+    if (!(/@/).test(email)) {
+      if ((/\s/).test(email)) {
+        email = ctrl.emailFromName(email);
+      } else {
+        email = ctrl.emailFromAlias(email);
+      }
+    }
+    console.log('after', email);
     Gouncer.authenticate(email, password).then($scope.onLogin, $scope.onLoginError);
   };
 
   $scope.logout = function() {
     NpolarApiSecurity.logout();
-    $route.reload();
+    //$route.reload();
   };
 
-};
+}
 
 module.exports = LoginController;

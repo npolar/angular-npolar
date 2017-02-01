3897e5b0aba0a984d5a48a31bc4f567c73bbb1c8
cnrdh <cnrdh@users.noreply.github.com>
2017-02-01 21:26:12 +0100

    Fixed name filter for aliases with name property

--- src/ui/filters/name.js
+++ src/ui/filters/name.js
@@ -10,21 +10,26 @@ module.exports = function(npolarPeople, npolarAliases) {
     let a = npolarAliases.find(a => a.alias.includes(email));
     if (a) {
       if (a.name) {
-        name = a.name;
+        name = a.name; // Name found
       } else {
         email = a.email; // Lookup in next block
       }
     }
 
-    // In people <= Person API?
-    let p = npolarPeople.people.find(p => p.email === email);
-    if (p && p.first_name && p.last_name) {
-      name = `${p.first_name} ${p.last_name}`;
-    } else {
+    if (typeof(name) !== 'string') { // Not alias with name property
+      // In people <= Person API?
+      let p = npolarPeople.people.find(p => p.email === email);
+      if (p) {
+        if (p.first_name && p.last_name) {
+          name = `${p.first_name} ${p.last_name}`;
+        }
+      }
+    }
+
+    if (typeof(name) !== 'string') {
       // Bail out => return input
       name = email;
     }
-
     return name;
   };
 };
